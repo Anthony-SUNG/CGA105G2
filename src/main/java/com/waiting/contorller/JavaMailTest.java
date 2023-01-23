6bd10d39c55d0645ba5b38c330a4e469fd3a9570
zx412563 <zxchuahua2417@gmail.com>
2023-01-23 18:34:27 +0800

    Update SendMmail

---
+++ src/main/java/com/waiting/contorller/JavaMailTest.java
@@ -0,0 +1,14 @@
+package com.waiting.contorller;
+
+
+public class JavaMailTest {
+	
+	public static void main(String[] args) {
+		Mailer mailer = new Mailer();
+		
+		mailer.send("Jim", "0988959937","zxchuahua2417@gmail.com", "哈哈哈");
+		
+		
+	}
+
+}
